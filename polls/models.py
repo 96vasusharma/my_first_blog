e5dfe4fb157b19f5764b494dd6cbb2438bfea12e
Vasu <96vasusharma@gmail.com>
2016-03-24 18:56:43 +0530

    Mysite app , first commit

---
+++ polls/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    #edit_date = models.DateTimeField(auto_now_add=False,auto_now=True)
+    def __str__(self):              # __unicode__ on Python 2
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+    	return self.choice_text
